18c914b1c4e97ae7d65821127a46921f0ce45691
Manuel <gtr2500@gmail.com>
2025-08-22 17:39:18 +0200

    Create tipi.js

---
+++ assets/data/tipi.js
@@ -0,0 +1,9 @@
+// assets/data/tipi.js
+export const TIPI = [
+  { code:'TUB',  label:'Tubo' },
+  { code:'LAM',  label:'Lamiera' },
+  { code:'STA',  label:'Staffa' },
+  { code:'ASS',  label:'Assieme' },
+  { code:'PZN',  label:'Piegato Z' },
+  { code:'PGL',  label:'Piegato L' }
+];
