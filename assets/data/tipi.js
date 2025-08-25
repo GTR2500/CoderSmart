7d2ff64648760e306131d84386ef67f77990125f
Manuel <gtr2500@gmail.com>
2025-08-25 10:18:29 +0200

    Delete assets/data/tipi.js

--- assets/data/tipi.js
+++
@@ -1,9 +0,0 @@
-// assets/data/tipi.js
-export const TIPI = [
-  { code:'TUB',  label:'Tubo' },
-  { code:'LAM',  label:'Lamiera' },
-  { code:'STA',  label:'Staffa' },
-  { code:'ASS',  label:'Assieme' },
-  { code:'PZN',  label:'Piegato Z' },
-  { code:'PGL',  label:'Piegato L' }
-];
